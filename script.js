a672d20ea38b61e5bb60e8985ffc17a2d90f309d
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-09-01 16:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// your JS code here
+let element = document.getElementById("level");
+let currentLevel = 0;
+
+function calculateLevel(node) {
+  if (node) {
+    currentLevel++;
+    calculateLevel(node.parentElement);
+  }
+}
+
+calculateLevel(element);
+
+alert(`The level of the element is: ${currentLevel}`);
